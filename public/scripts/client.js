fbce82db4e02d15ed365d3c628b7da5cdceee605
Luca Properzi <abramoproperzi@gmail.com>
2020-11-17 23:06:35 +0000

    implemented character count

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  $('textarea').on('keyup', function() {
+    // let remain = 140 - $(this).val().length;
+    $('.counter').html(140 - $(this).val().length);
+
+    if (remain < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'grey');
+    }
+  });
+});
